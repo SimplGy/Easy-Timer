4ea78a6f32508f435315189adb6f33e710e830d0
Eric Miller <hi@simple.gy>
2015-03-04 17:13:27 +0700

    seconds always show a leading zero. also attempting to play a system sound but the path isn't resolving

--- ETTimerRectangle.swift
+++ ETTimerRectangle.swift
@@ -1,85 +1,74 @@
 //
 //  ETTimer.swift
-//  Easy Timer
+//  Extends UIView.
+//  * Draws the rectangle that represents remaining time.
+//  * Creates the timer
+//  * Displays the numeric time
+//  * Reacts to taps and drags by changing remaining time
 //
 //  Created by Eric on 6/8/14.
 //  Copyright (c) 2014 Simple Guy. All rights reserved.
 //
 
 import UIKit
+import AudioToolbox
 
 class ETTimerUIView: UIView {
     
-    // The default boilerplate has a useless signature for init. bug? TODO: something I don't understand.
-    //    init(frame: CGRect) {
-    //        super.init(frame: frame)
-    //        // Initialization code
-    //
-    //        println("custom view")
-    //    }
-    
-    
-    // TODO:
-    // o Color by sinebow
-    // o While dragging, snap to nearest 5 min increment
-    // o Change color by time remaining from set (not total)
-    // o fit seconds to minutes
-    
     @IBOutlet var timeDisplay : UILabel!
     @IBOutlet var secDisplay : UILabel!
-    
-    let limit: CGFloat       = 60.0 * 60.0 // a 60 minute timer
-    let interval: CGFloat    = 1.0
-    var percentage: CGFloat  = 0.0
-    var remaining: CGFloat   = 0.0
-    
 
-//    var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("tick"), userInfo: nil, repeats: true)
-//    var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("tick"), userInfo: nil, repeats: true)
+    let limit: CGFloat         = 60.0 * 60.0    // What's the maximum amount of time you can set a timer for?
+    let tickAmount: CGFloat    = 1.0            // 1.0 means: one tick per second
+    let timerInterval: CGFloat = 0.01           // Should be`tickAmount`. Smaller values are for debugging a fast clock.
+    var percentage: CGFloat    = 0.0            // How much time is left, as a percentage of `limit`
+    var remaining: CGFloat     = 0.0            // How much time is remaining right now?
+    var prevRemaining: CGFloat = 0.0            // On the last tick, how much time was remaining?
+    var setTime: CGFloat       = 0.0            // How much time has the user asked for a timer to run?
 
-        
     required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder) // this sounds like a superhero ring. TODO: What is an NSCoder and who invited it to the party?
-        println("In ETTimerRectangle")
+        super.init(coder: aDecoder) // this sounds like a superhero ring. TODO: What is an NSCoder and who invited it to the
         var timer = NSTimer.scheduledTimerWithTimeInterval(
-            NSTimeInterval(interval),
+            NSTimeInterval(timerInterval),
             target: self,
             selector: "tick",
             userInfo: nil,
             repeats: true)
-        timer.tolerance = Double(interval) * 0.1 // perf gain by allowing fuzzy times.
+        timer.tolerance = Double(timerInterval) * 0.1 // perf gain by allowing fuzzy times.
     }
 
-
-
-    
     func setTimeDisplay() {
         timeDisplay.text = String(Int(remaining) / 60)
-        secDisplay.text = ":" + String(Int(remaining % 60))
+        var seconds = String(Int(remaining % 60))
+        if countElements(seconds) == 1 {
+            seconds = "0" + seconds
+        }
+        secDisplay.text = seconds
         self.setNeedsDisplay()
     }
     
-
     // Every step of the timer calls this method
     func tick() {
-        remaining = round(remaining)
-        remaining -= interval
-        if remaining <= 0 {
-            // stop timer
-            timeDisplay.text = ""
-            secDisplay.text = ""
-        } else {
+        // Timer is running
+        if remaining > 0 {
+            prevRemaining = remaining
+            remaining = round(remaining)
+            remaining -= tickAmount
             percentage = remaining / limit
-            println(remaining)
             setTimeDisplay()
+        
+        // Timer has just reached zero
+        } else if remaining != prevRemaining {
+            remaining = 0.0
+            prevRemaining = 0.0
+            onTimerFinished()
+            setTimeDisplay()
+
+        // idle timer.
+        } else {
+            // nuttin'
         }
     }
-    
-    
-    
-
-    
-    
     
     // Given a position, set the time remaining on the timer
     func setRemainingTimeByPos(yOffset: CGFloat) {
@@ -131,6 +120,27 @@ class ETTimerUIView: UIView {
         rect = CGRectMake(0, top, rect.width, height)
         CGContextFillRect(context, rect)
         
+    }
+    
+    // When the timer has finished, "ring" it
+    func onTimerFinished(){
+        
+        println("onTimerFinished")
+        
+        // Create the sound object
+        let filePath = NSBundle.mainBundle().pathForResource("Tock", ofType: "aiff")
+        // Make sure it found a path--sometimes it doesn't
+        if (filePath == nil) {
+            println("Can't get the path to a sound file")
+        } else {
+            let fileURL = NSURL(fileURLWithPath: filePath!)
+            var soundID:SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(fileURL, &soundID)
+            AudioServicesPlaySystemSound(soundID)
+        }
+        
+        // Dispose
+//        AudioServicesDisposeSystemSoundID(soundID)
     }
     
 }
